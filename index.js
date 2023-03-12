434815b6956e757aacda34aaafdcd2095577e3e3
samc995 <Sam995@gmail.com>
2023-03-11 16:19:16 -0800

    Finished project

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('console.table')
 
 const utils = require('util')
 const { start } = require('repl')
+const { title } = require('process')
 db.query = utils.promisify(db.query)
 
 async function startApp() {
@@ -89,19 +90,104 @@ startApp()
 }
 
 async function addDepartment() {
-    const answer = await inquirer.prompt([
+  inquirer.prompt([
         {
             type: "input",
-            name: "department",
+            name: "name",
             message: "what is the name of the new department?"
         }
-    ])
-    const result = await db.query("insert into department (name) values=(?)" , [answer.department])
-    console.log("your department was added!")
-    startApp()
+    ]).then(res => {
+      var name = res;
+      db.query("INSERT INTO department SET ?", name)
+      .then(() => console.log(`Added ${name.name} to the database`))
+      .then(() => startApp())
+    })
 }
 
+async function addRole() {
+  db.query("Select * from department").then((rows) => {
+    console.log(rows)
+const choices = rows.map (({id , name })=> {
+  return {
+    name: name,
+    value: id
+  }
+})
+inquirer.prompt([
+  {
+      type: "input",
+      name: "title",
+      message: "what is the name of the new role?"
+  },
+  {
+    type: "input",
+    name: "salary",
+    message: "what is the salary of the new role?"
+},
+{
+  type: "list",
+  name: "department_id",
+  message: "what is the department ID of the new role?",
+  choices: choices
+}
+]).then(role => {
+  console.log (role)
+db.query(`INSERT INTO role (title, salary, department_id) values('${role.title}', ${role.salary}, ${role.department_id})`)
+.then(() => console.log(`Added new role to the database`))
+.then(() => startApp())
+})
+  })
+}
 
+  async function addEmployee() {
+    const data = await db.query("Select * from employee")
+    const employees = data.map (({first_name, last_name, id})=> {
+      return {
+        name: `${first_name} ${last_name} `,
+        value: id
+      }
+    })
+    db.query("Select * from role").then((rows) => {
+  const choices = rows.map (({id , title })=> {
+    return {
+      name: title,
+      value: id
+    }
+  })
+  
+  // db.query("Select * from employee").then((employees) => {
+
+  // })
+  inquirer.prompt([
+    {
+        type: "input",
+        name: "first_name",
+        message: "what is the first name of the new employee?"
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "what is the last name of the new employee?"
+  },
+  {
+    type: "list",
+    name: "role_id",
+    message: "what is the role ID of the new employee?",
+    choices: choices
+  },
+  {
+    type: "list",
+    name: "manager_id",
+    message: "what is the manager ID of the new employee?",
+    choices: employees
+  }
+  ]).then(employee => {
+  db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) values('${employee.first_name}', '${employee.last_name}', ${employee.role_id}, ${employee.manager_id})`)
+  .then(() => console.log(`Added new employee to the database`))
+  .then(() => startApp())
+  })
+    })
+  }
 
 
 startApp()
